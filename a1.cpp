c4a43dc466a35e3a9b6c58f420f72f0cfba39b28
jorcallegar <33935435+jorcallegar@users.noreply.github.com>
2020-02-04 18:28:25 +0100

    Seguimos

--- a1.cpp
+++ a1.cpp
@@ -5,13 +5,29 @@
 
 Programa de AC
 
+Variables:
+n: tamaño de la imagen
+entradaTipoDePeticion : nos dice si quiere t o quiere d 
+'t' es tiempo
+'d' es datos
+
+
+
 *******************************************/
 
 using namespace std;
 int main()
 {
+	int n; 
+	string entradaTipoDePeticion;
+		
+	if (entradaTipoDePeticion != "t" or entradaTipoDePeticion != "d"){ 
+		cout << "Introduce c o d";
+		cin >> entradaTipoDePeticion;
+	}
 
-    for(int i=50;i<=100;i+=2)
+	
+	for(int i=50;i<=100;i+=2)
     {
 	
     	for(int j=0; j<=100;j+=2)
