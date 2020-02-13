20682c1a10d34f997a176aab05032cdd5d52cf4f
jorcallegar <33935435+jorcallegar@users.noreply.github.com>
2020-02-13 21:27:49 +0100

    Revisionreal

--- a1.cpp
+++ a1.cpp
@@ -1,40 +1,78 @@
-#include "iostream"
-#include "stdlib.h"
+
+#include <iostream>
+#include <stdlib.h>
 #include <math.h>
 #include <string.h>
 #include <time.h>
 #include <sys/time.h>
-/*****************************************
-Programa de AC
-Variables:
-n: tamaÃ±o de la imagen
-entradaTipoDePeticion : nos dice si quiere 't' o quiere 'd' 
-'t' es tiempo
-'d' es datos
-x: x de la matriz generada
-y: y de la matriz generada
-i,k,j: son variables iterativas
-s: numero de filtros
-m: dimensiÃ³n de los filtros
-C[20][5][5]: bateria de filtros
-B[s][n][n]: Array resultado de la convoluciÃ³n de la imagen
-R[s][n/2][n/2]: Resultado del pooling
-M[n/2][n/2]: REsultado del promediado. Salida del algoritmo
-*******************************************/
+#include <fstream>
 
 using namespace std;
-int main(int argc, char *argv[])
+
+/****************************************************************************************/ /*
+*
+*										Programa de AC
+*	Variables simples:
+*		n: tamaño de la imagen
+*		tipo_resultado: nos dice si quiere 't' o quiere 'd' 
+*		't' es tiempo
+*		'd' es datos
+*		x: x de la matriz generada
+*		y: y de la matriz generada
+*		i,k,j: son variables iterativas
+*		s: numero de filtros
+*		m: dimensiÃ³n de los filtros
+*
+*	Variables complejas:
+*		C[20][5][5]: bateria de filtros
+*		B[s][n][n]: Array resultado de la convolución de la imagen
+*		R[s][n/2][n/2]: Resultado del pooling
+*		M[n/2][n/2]: REsultado del promediado. Salida del algoritmo
+*
+*/ /*****************************************************************************************/
+
+
+int main( int argc, char *argv[] )
 {
 	int n;
-	string entradaTipoDePeticion;
-	int x, y, k, i, j, z;
+	bool tipo_resultado;
+	
+	/******************
+	
+	True: Datos
+	False: Tiempos
+	
+	*******************/
+	
+	
+	int x, y, z;						//variables para la matriz
+	int k, i, j;						//Variables iteracciones
+	
+	/*****************
+	Parte inicialización del main
+	*****************/
+	
+	if ( argc != 2 ){
+		n=120;
+		tipo_resultado = false;
+	}
+	else {
+		n = atoi(argv[1]);
+		
+		if(atoi(argv[2] )== 'd')
+			tipo_resultado = true;
+		else
+			tipo_resultado = false; 
+	}
+
+	
 	float  A[n][n];
 	float C[20][5][5];
 	
 	int s = 20;
 	int m = 5;
-	double d;
-	double B[s][n][n];
+	
+	float B[s][n][n];
 	float R[s][n/2][n/2];
 	float M[n/2][n/2];
 	
@@ -42,12 +80,6 @@ int main(int argc, char *argv[])
 	
 	long time;
 	
-	if(argc == 2)
-		n =120; 
-	
-	else
-		n = atoi(argv[2]);
-
 	
 
 	
@@ -66,18 +98,18 @@ int main(int argc, char *argv[])
 	}**/
 	
 	//////////////////////////////////////////////////
-	/***********************
+	/*********
 	Generacion de matriz
-	***********************/
+	*********/
 	
 	
 	
 	
 	
-	for(x=0 ; x<n; x+=1)
+	for(x=0 ; x<n; x++)
     {
 	
-    	for(y=0; y<n; y+=1)
+    	for(y=0; y<n; y++)
 		{
 				
     		A[x][y]= ((x*y + x + y) / (3 *n*n))*10^3;
@@ -86,10 +118,10 @@ int main(int argc, char *argv[])
 	}
 	
 	
-	/*********************************
+	/***********
 	Generacion de la bateria de filtros
 	Hemos generado  C[k][i][j] que es una bateria de filtros
-	**********************************/
+	************/
 	
 	gettimeofday(&tiempo0, NULL);
 	
@@ -99,26 +131,30 @@ int main(int argc, char *argv[])
 		
 			for(j=0; j<m;j++){
 				C[k][i][j]= (rand()*pow(-1,j+k))/RAND_MAX;
+				cout << C[k][i][j] << endl;
 			}
 		}
 	}
 	
     
-	/***********************************
+	/*************
 	1.Calculo de la convolucion de la imagen
 	
-	**************************************/
-	for(int z=0; z<s ; z++)
+	**************/
+	for(z=0; z<s ; z++)
 	{
  		for(x=2; x<n-2; x++)
  		{
 			for(y=2; y<n-2; y++)
 			{
+				//Lo inicializo a 0, porque cada uno va a tener un valor distinto, los cuales empezarán evidentemete a 0
+				B[z][x][y] = 0;						//Cambio -> Añadido
 				for(i=-(m/2); i<(m/2); i++)
             	{
  					for(j=-(m/2); j<(m/2); j++)
          			{
-            	 		B[z][x][y]= A[x+i][y+j] * C[z][((m/2) + i)][((m/2) + j)];
+         				//He añadido el B[z][x][y] en el sumatorio, pues al final todo es una operación de lo va sumando
+            	 		B[z][x][y] += A[x+i][y+j] * C[z][((m/2) + i)][((m/2) + j)]; // Cambio (modificación)
             	 		
 					}		  
        			}	   
@@ -128,10 +164,10 @@ int main(int argc, char *argv[])
 
 	
 	
-	/********************************
+	/************
 	2.Aplicacion de la funcion no lineal
-	*********************************/
-	
+	***********/
+	system("pause");
 	for(int z=0; z<s ; z++)
 	{
 	
@@ -140,19 +176,21 @@ int main(int argc, char *argv[])
     	{
         	for(y=2; y<n-2; y++)
         	{
-        		d=(B[z][x][y]);
-            	//B[z][x][y]=(1/(1 + pow(2.718281828459045,d )));  
-            	cout << d;
-            	cout << endl;
+        		//cout << B[z][x][y] << "         ";
+            	B[z][x][y]= 1/(1 + 1/pow(2.718281828459045, B[z][x][y]));  
+            	//cout << B[z][x][y];
+            	//cout << endl;
+            	//pause(1);
         	}
     	}
 	}
 
-	
-	/***********************************
+	cout << "Parte Pulling" << endl;
+	system("pause");
+	/*************
 	3.Pooling
-	************************************
-	
+	************/
+	cout << "*********************************" << endl;
 	int max;
 	for (z=0; z<s; z++){
 		for(x=0; x<n/2; x++){
@@ -171,10 +209,13 @@ int main(int argc, char *argv[])
 		}
 	}
 	
+	cout << "*********************************" << endl;
 	
-	/***************************************
+	cout << "Parte Promedio" << endl;
+	system("pause");
+	/*************
 	4.Promediado
-	****************************************
+	**************/
 	
 	int media = 0;
 	for (z=0; z<s; z++){
@@ -192,6 +233,7 @@ int main(int argc, char *argv[])
 	time= tiempo1.tv_sec * 1000000-tiempo0.tv_sec * 1000000;
 	
 	system("PAUSE");
+    
     return 0;
-    **/
 }
+
